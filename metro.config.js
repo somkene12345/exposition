89a64260807d2256a7891257e4915b187fcdd4df
somkenenna okechukwu <somkeneokechukwu@outlook.com>
2024-03-27 13:53:11 +0100

    please work

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
 // metro.config.js
-module.exports = {
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+  return {
+    ...defaultConfig,
     // Specify the entry file for your React Native app
     entry: './App.js',
-};
+  };
+})();

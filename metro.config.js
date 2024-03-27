86e4c20c67d6e73053fee9f5f38a6ade8f75e03c
somkene12345 <153438701+somkene12345@users.noreply.github.com>
2024-03-27 14:07:45 +0100

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,11 @@
-// metro.config.js
-const { getDefaultConfig } = require('metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
-  return {
-    ...defaultConfig,
-    // Add any additional configuration here
-  };
-})();
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
